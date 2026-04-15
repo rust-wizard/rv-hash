8889aa919cb83bba23fd59c7fc971e62a17508fe
wizard <wizard@rust.com>
2026-04-15 15:52:03 +0800

    feat: human readable name

--- x86/src/main.rs
+++ x86/src/main.rs
@@ -1,5 +1,6 @@
 use blake2::{Blake2s256, Digest};
 use hex_literal::hex;
+use sha2::{Digest as Sha2Digest, Sha256};
 
 fn main() {
     let mut hasher = Blake2s256::new();
@@ -12,5 +13,11 @@ fn main() {
     9aec6806794561107e594b1f6a8a6b0c92a0cba9acf5e5e93cca06f781813b0b
 "
         )[..]
+    );
+
+    let hash = Sha256::digest(b"hello world");
+    assert_eq!(
+        hash,
+        hex!("b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9")
     );
 }
